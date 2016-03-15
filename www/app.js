e7bed5b2ac7c652afdd29f88f2c3d97cb158885e
Frank <flyingfz@163.com>
2016-03-15 15:05:12 +0800

    删除多余代码

--- www/app.js
+++ www/app.js
@@ -1,13 +1,10 @@
 /**
  * Created by yzsoft on 16/3/15.
  */
-//import Login from './js/page/login/login.js';
+//import {initModel} from './js/page/login/login.js';
 const Login =require('./js/page/login/login.js');
 jQuery(document).ready(function(){
     "use strict";
-    console.log('hello');
-    console.log(Login);
     Login.initModel();
-    //Login.initModel();
 });
 
