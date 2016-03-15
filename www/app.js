c33fb2f61fc26849f17ad4c97f6d036042bc8653
Frank <flyingfz@163.com>
2016-03-15 14:52:13 +0800

    新增项目入口页

---
+++ www/app.js
@@ -0,0 +1,13 @@
+/**
+ * Created by yzsoft on 16/3/15.
+ */
+//import Login from './js/page/login/login.js';
+const Login =require('./js/page/login/login.js');
+jQuery(document).ready(function(){
+    "use strict";
+    console.log('hello');
+    console.log(Login);
+    Login.initModel();
+    //Login.initModel();
+});
+
