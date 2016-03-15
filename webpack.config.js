aec2b5ef4d215d65dce34f0549280479191d606c
Frank <flyingfz@163.com>
2016-03-15 14:50:59 +0800

    修正webpack配置文件

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 /**
  * Created by yzsoft on 16/3/15.
  */
-var path=rquire('path');
+var path=require('path');
 module.exports={
   entry:path.resolve(__dirname,'www/app.js'),
   output:{
-     path:path.resolve(__dirname,'build'),
+     path:path.resolve(__dirname,'www/build'),
      filename:'bundle.js'
   }
 };
